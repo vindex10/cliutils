69841bf993de68c1ab80983ec6dca690d2401de4
Victor Ananyev <vindex10@gmail.com>
2023-03-27 17:11:14 +0200

    add kubectl

---
+++ apprepo/kubectl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf kubectl.bak
+mv kubectl kubectl.bak
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
