ca35e6e2d43a953ee5ce364df4247dc249d660fc
vindex10 <vindex10@gmail.com>
2024-10-12 18:47:38 +0200

    add musescore, sfzdir and asus kbd fix

---
+++ apprepo/musescore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+name=$(gh-get-link 'musescore/MuseScore' '-x86_64.AppImage$' -n)
+url=$(gh-get-link 'musescore/MuseScore' '-x86_64.AppImage$')
+
+rm -f *.bak
+for i in MuseScore-Studio-*-x86_64.AppImage; do
+    mv $i $i.bak
+done
+
+wget $url
+chmod +x "$name"
+ln -s "$name" musescore
